f97a9b2810b7563d5d787ea6bdac8057392db855
Francine Hahn <fran_hahn@hotmail.com>
2023-09-18 18:49:16 -0300

    feat: send messages step by step

---
+++ src/sendMessages/sendMessages.py
@@ -0,0 +1,11 @@
+
+
+class sendMessages:
+    #pegar lista de usuários com a variável messages_to_be_answered == True
+    #pegar o message_sender e escrever no input de pesquisar
+    #clica em cima do usuário e abre a conversa
+    #pega o último text que é o da AI e cola no input de enviar mensagem
+    #clicar no enter ou botão de enviar
+    #fazer um update no banco de dados da variável messages_to_be_answered == False
+
+    
