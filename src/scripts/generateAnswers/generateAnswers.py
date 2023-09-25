5737697df6f08f80ca38159f2c3126e5768d3632
Francine Hahn <fran_hahn@hotmail.com>
2023-09-25 11:58:37 -0300

    feat: I included buttons in the streamlit so that we can run the scripts from there

--- src/scripts/generateAnswers/generateAnswers.py
+++ src/scripts/generateAnswers/generateAnswers.py
@@ -5,7 +5,7 @@ import streamlit as st
 from datetime import datetime
 from src.repository.repository import Repository
 from src.service.openAI import openAI
-
+import subprocess
 
 def init():
     st.set_page_config(
@@ -19,73 +19,107 @@ def main():
 
     users_to_be_answered = Repository().get_users_by_need_to_generate_answer()
 
-    if len(users_to_be_answered) > 0:
-        doc_id = users_to_be_answered[0].id
-        user = users_to_be_answered[0].to_dict()
-        all_messages = user["messages"]
+    def run_subprocess(script):
+        process = subprocess.Popen(script, shell=True, stderr=subprocess.PIPE)
+        process.wait()
+        stderr = process.stderr.read().decode('utf-8')
 
-        #getting all the messages that were sent after the last message the seller sent (CAIO TERÁ QUE MUDAR PARA O NOME DELE)
-        #these messages will be used in the gpt prompt
-        user_last_messages = []
-        for message in list(reversed(all_messages)):
-            if message["sender"] != " Fran Hahn: ":
-                user_last_messages.append(message["text"])
-            else:
-                break
+        if process.returncode != 0:
+            st.error(f"Ocorreu um erro: {stderr}")
+        else:
+            st.success("Script executado com sucesso!")        
+
+    if len(users_to_be_answered) == 0:
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            if st.button("Enviar 1ª mensagem", key="script1"):
+                st.info("Vá para o whatsapp web SEM A ABA DE INSPECIONAR ABERTA em até 4 segundos")
+
+                #CAIO mudar pelo caminho no teu pc
+                run_subprocess("python c:/Users/fran_/Documents/EMPRESA/CRIA.AI/CriaBot/src/run/runFirstMessageScript.py")
         
-        user_last_messages = list(reversed(user_last_messages))
-        gpt_answer = openAI(user_last_messages)
+        with col2:
+            if st.button("Extrair mensagens", key="script2"):
+                st.info("Vá para o whatsapp web COM A ABA DE INSPECIONAR ABERTA em até 4 segundos")
+                
+                #CAIO mudar pelo caminho no teu pc
+                run_subprocess("python c:/Users/fran_/Documents/EMPRESA/CRIA.AI/CriaBot/src/run/runExtractMessagesScript.py")
 
-        if user["stage"] < 3:
-            with st.sidebar:
-                st.info("Baseando-se nas respostas do usuário, você quer prosseguir com o envio de mensagens?")
+        with col3:
+            if st.button("Responder leads", key="script4"):
+                st.info("Vá para o whatsapp web SEM A ABA DE INSPECIONAR ABERTA em até 4 segundos")
 
-                col1, col2 = st.columns(2)
-
-                with col1:
-                    if st.button("Não", key=f"reject_{doc_id}"):
-                        Repository().update_stage_number(doc_id, 0)
-                        Repository().update_need_to_generate_answer(doc_id, {"need_to_generate_answer": False})
-                        Repository().update_need_to_send_answer(doc_id, {"need_to_send_answer": False})
-                        st.experimental_rerun()
-
-                with col2:
-                    if st.button("Sim", key=f"accept_{doc_id}"):
-                        if user["stage"] < 3:
-                            Repository().update_stage_number(doc_id, user["stage"] + 1)
-                        Repository().update_need_to_generate_answer(doc_id, {"need_to_generate_answer": False})
-                        Repository().update_need_to_send_answer(doc_id, {"need_to_send_answer": True})
-                        st.experimental_rerun()
-                        
-        elif user["stage"] > 3:
-            with st.sidebar:
-                with st.form("my_form"):
-                    st.text_area(label="Resposta", value=gpt_answer, height=400, key="edited_gpt_answer")
-
-                    def handle_submit():
-                        edited_gpt_answer_value = st.session_state.edited_gpt_answer
-                        all_messages.append({
-                            "date": datetime.now().strftime("%H:%M, %d/%m/%Y"),
-                            "sender": " Fran Hahn: ", #CAIO, mudar pelo seu nome
-                            "text": edited_gpt_answer_value
-                        })
-
-                        Repository().update_messages_array(doc_id, all_messages)
-                        Repository().update_need_to_generate_answer(doc_id, {"need_to_generate_answer": False})
-                        Repository().update_need_to_send_answer(doc_id, {"need_to_send_answer": True})
-
-                    st.form_submit_button("Aceitar", on_click=handle_submit)
-                    
-                if st.button("Rejeitar", key=f"reject_{doc_id}"):
-                    print("REJECTED GPT MESSAGE")
-                    
-        # Rendering the message history between the lead and the seller
-        for message in all_messages:
-            st.info(f"{message['sender']} {message['text']}")
+                #CAIO mudar pelo caminho no teu pc
+                run_subprocess("python c:/Users/fran_/Documents/EMPRESA/CRIA.AI/CriaBot/src/run/runSendMessagesScript.py")
 
     else:
-        st.write("Todos os usuários foram respondidos.")
-        st.stop()
+        if len(users_to_be_answered) > 0:
+            doc_id = users_to_be_answered[0].id
+            user = users_to_be_answered[0].to_dict()
+            all_messages = user["messages"]
+
+            if user["stage"] < 3:
+                with st.sidebar:
+                    st.info("Baseando-se nas respostas do usuário, você quer prosseguir com o envio de mensagens?")
+
+                    col1, col2 = st.columns(2)
+
+                    with col1:
+                        if st.button("Não", key=f"reject_{doc_id}"):
+                            Repository().update_stage_number(doc_id, 0)
+                            Repository().update_need_to_generate_answer(doc_id, {"need_to_generate_answer": False})
+                            Repository().update_need_to_send_answer(doc_id, {"need_to_send_answer": False})
+                            st.experimental_rerun()
+
+                    with col2:
+                        if st.button("Sim", key=f"accept_{doc_id}"):
+                            Repository().update_stage_number(doc_id, user["stage"] + 1)
+                            Repository().update_need_to_generate_answer(doc_id, {"need_to_generate_answer": False})
+                            Repository().update_need_to_send_answer(doc_id, {"need_to_send_answer": True})
+                            st.experimental_rerun()
+                            
+            elif user["stage"] > 3:
+                #getting all the messages that were sent after the last message the seller sent (CAIO TERÁ QUE MUDAR PARA O NOME DELE)
+                #these messages will be used in the gpt prompt
+                user_last_messages = []
+                for message in list(reversed(all_messages)):
+                    if message["sender"] != " Fran Hahn: ":
+                        user_last_messages.append(message["text"])
+                    else:
+                        break
+                
+                user_last_messages = list(reversed(user_last_messages))
+                gpt_answer = openAI(user_last_messages)
+
+                with st.sidebar:
+                    with st.form("my_form"):
+                        st.text_area(label="Resposta", value=gpt_answer, height=400, key="edited_gpt_answer")
+
+                        def handle_submit():
+                            edited_gpt_answer_value = st.session_state.edited_gpt_answer
+                            all_messages.append({
+                                "date": datetime.now().strftime("%H:%M, %d/%m/%Y"),
+                                "sender": " Fran Hahn: ", #CAIO, mudar pelo seu nome
+                                "text": edited_gpt_answer_value
+                            })
+
+                            Repository().update_messages_array(doc_id, all_messages)
+                            Repository().update_need_to_generate_answer(doc_id, {"need_to_generate_answer": False})
+                            Repository().update_need_to_send_answer(doc_id, {"need_to_send_answer": True})
+
+                        st.form_submit_button("Aceitar", on_click=handle_submit)
+                        
+                    if st.button("Rejeitar", key=f"reject_{doc_id}"):
+                        print("REJECTED GPT MESSAGE")
+                        
+            # Rendering the message history between the lead and the seller
+            for message in all_messages:
+                st.info(f"{message['sender']} {message['text']}")
+
+        else:
+            st.write("Todos os usuários foram respondidos.")
+            st.stop()
     
 if __name__ == '__main__':
     main()
--- tests/firstMessageTest.py
+++ tests/firstMessageTest.py
@@ -7,12 +7,13 @@ from unittest.mock import MagicMock
 from src.scripts.firstMessage.firstMessage import FirstMessage
 from mocks.repositoryMock import repository_mock
 
-    
+#One phone number is in the correct format and the other is not
+#so, only the correct one should be inserted in the database
 def test_send_first_messages_to_correct_numbers():
     pyautogui_module=MagicMock()
     keyboard_module=MagicMock()
     repository=repository_mock
-    file_path = "c:/Users/fran_/Documents/EMPRESA/CRIA.AI/CriaBot/tests/mocks/files/correctPhoneNumbersMock.csv"
+    file_path = "c:/Users/fran_/Documents/EMPRESA/CRIA.AI/CriaBot/tests/mocks/files/phoneNumbersMock.csv"
 
     FirstMessage(
         pyautogui_module=pyautogui_module,
@@ -22,20 +23,5 @@ def test_send_first_messages_to_correct_numbers():
     ).open_conversation()
 
     assert pyautogui_module.assert_any_call
-    repository.insert_new_document.assert_called()
-
-def test_send_first_messages_to_incorrect_numbers():
-    pyautogui_module=MagicMock()
-    keyboard_module=MagicMock()
-    repository=repository_mock
-    file_path = "c:/Users/fran_/Documents/EMPRESA/CRIA.AI/CriaBot/tests/mocks/files/incorrectPhoneNumbersMock.csv"
-
-    FirstMessage(
-        pyautogui_module=pyautogui_module,
-        keyboard_module=keyboard_module,
-        repository=repository,
-        file_path=file_path
-    ).open_conversation()
-
-    assert pyautogui_module.assert_not_called
-    repository.insert_new_document.assert_not_called()
+    repository.get_user_by_name.assert_called_once()
+    repository.insert_new_document.assert_called_once()
